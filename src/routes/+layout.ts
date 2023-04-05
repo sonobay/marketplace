72c17fa0f677f34d5f557818180f7a3e24413c91
Stuart Kuentzel <s.w.kuentzel@gmail.com>
2023-04-06 00:21:27 +0900

    logs fetching devices

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -4,11 +4,15 @@ import type { Device } from '$lib/types/device';
 export const load = async () => {
 	const { apiEndpoint } = variables;
 
+	console.log('fetching devices...');
+
 	const res = await fetch(`${apiEndpoint}/devices`);
 	if (!res.ok) {
 		throw new Error(`error fetching devices`);
 	}
 	const devices = (await res.json()) as Device[];
+
+	console.log('returning devices...');
 
 	return { devices };
 };
