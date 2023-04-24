75d3e95c0f078685c6b06b6a1d53f0584c2dd3aa
Stuart Kuentzel <s.w.kuentzel@gmail.com>
2023-04-24 19:00:46 +0900

    device tags init

---
+++ src/lib/components/DeviceMidiSearch.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	let searchTerm = '';
+
+	const handleSearch = () => {
+		if (searchTerm.length < 3) {
+			return;
+		}
+
+		console.log('searchTerm is: ', searchTerm);
+	};
+</script>
+
+<div>
+	<input bind:value={searchTerm} />
+
+	<button disabled={searchTerm.length < 3} on:click={(_) => handleSearch()}>Search</button>
+</div>
