fe13e711f105f624578654304330703baf449845
appelskrutt34 <ebba1430@hotmail.com>
2023-06-21 07:08:01 +0200

    new pagination buttons

--- src/lib/components/icons/CaretRight.svelte
+++ src/lib/components/icons/CaretRight.svelte
@@ -1,11 +1,12 @@
 <script>
-	export let color = '#414141';
-	export let width = '24px';
-	export let height = '24px';
+	export let color = 'rgb(127 127 127)';
+	export let width = '18px';
+	export let height = '18px';
 </script>
 
-<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" fill={color} {width} {height}
-	><!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path
-		d="M278.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-160 160c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L210.7 256 73.4 118.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l160 160z"
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 47.62 83.11" {width} {height}
+	><path
+		fill={color}
+		d="m0,76.92c2.11,2.09,4.16,4.13,6.24,6.19,13.76-13.82,27.54-27.67,41.38-41.57C33.81,27.66,20.02,13.81,6.27,0,4.19,2.06,2.12,4.11.02,6.18c11.71,11.77,23.42,23.53,35.19,35.35C23.47,53.33,11.75,65.11,0,76.92Z"
 	/></svg
 >
