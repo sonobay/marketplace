e133e664e556860b286e192f3221dbbaf3971f07
Stuart Kuentzel <s.w.kuentzel@gmail.com>
2023-06-22 22:00:47 +0900

    adds logging to fetch listing

--- src/routes/listing/[address]/+page.ts
+++ src/routes/listing/[address]/+page.ts
@@ -9,16 +9,30 @@ export const load = async ({ params }: LoadEvent) => {
 		throw new Error('No ID found');
 	}
 
-	const [price, seller, tokenId, listed] = await Promise.all([
-		fetchPrice(address),
-		fetchSeller(address),
-		fetchTokenId(address),
-		fetchListed(address)
-	]);
+	// const [price, seller, tokenId, listed] = await Promise.all([
+	// 	fetchPrice(address),
+	// 	fetchSeller(address),
+	// 	fetchTokenId(address),
+	// 	fetchListed(address)
+	// ]);
+
+	const price = await fetchPrice(address);
+	console.log('price is: ', price);
+	const seller = await fetchSeller(address);
+	console.log('seller is: ', seller);
+	const tokenId = await fetchTokenId(address);
+	console.log('tokenId is: ', tokenId);
+	const listed = await fetchListed(address);
+	console.log('listed is: ', listed);
 
 	const availableAmount = await fetchBalanceOf(address, tokenId.toNumber());
+	console.log('availableAmount is: ', availableAmount.toString());
+
 	const totalAmount = await fetchTotalReceived(address, tokenId.toNumber());
+	console.log('totalAmount is: ', totalAmount.toString());
+
 	const ipfsMetadata = await get(tokenId.toString());
+	console.log('ipfsMetadata is: ', ipfsMetadata);
 
 	return {
 		availableAmount,
