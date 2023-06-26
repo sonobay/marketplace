ef0972faa3bc0d30496bffdd121a4f83dd06dcbd
Stuart Kuentzel <s.w.kuentzel@gmail.com>
2023-06-27 00:28:31 +0900

    cleanup

--- src/lib/utils/listing.contract.ts
+++ src/lib/utils/listing.contract.ts
@@ -1,26 +1,11 @@
 import * as listingArtifact from '$lib/data/artifacts/contracts/listing/Listing.sol/Listing.json';
-import { Contract, getDefaultProvider, Signer, BigNumber, providers } from 'ethers';
+import { Contract, getDefaultProvider, Signer, BigNumber } from 'ethers';
 import { environment } from '$lib/env';
 
 export const listingContract = async (address: string, signer?: Signer) => {
 	const { providerEndpoint } = environment;
-	console.log('provider endpoint: ', providerEndpoint);
-	console.log('trimmed endpoint: ', providerEndpoint.trim());
-	console.log('address: ', address);
-	console.log('signer: ', signer);
 
-	console.log('entire env is: ', environment);
-
-	try {
-		return new Contract(
-			address,
-			listingArtifact.abi,
-			signer ?? getDefaultProvider(providerEndpoint)
-		);
-	} catch (error) {
-		console.log('error is: ', error?.toString());
-		throw new Error(error?.toString());
-	}
+	return new Contract(address, listingArtifact.abi, signer ?? getDefaultProvider(providerEndpoint));
 };
 
 export const buyItems = async ({
@@ -47,41 +32,26 @@ export const buyItems = async ({
 };
 
 export const fetchPrice = async (address: string): Promise<BigNumber> => {
-	console.log('fetching price');
 	const contract = await listingContract(address);
-	console.log('fetchPrice contract initialized');
-	console.log('contract.address is: ', contract.address);
-	const _prov = contract.provider;
-	console.log('network is: ', await _prov.getNetwork());
 	const price = await contract.price();
-	console.log('price to return is: ', price.toString());
 	return price;
 };
 
 export const fetchSeller = async (address: string): Promise<string> => {
-	console.log('fetching seller');
 	const contract = await listingContract(address);
-	console.log('fetchSeller contract initialized');
 	const seller = await contract.seller();
-	console.log('seller is: ', seller);
 	return seller;
 };
 
 export const fetchTokenId = async (address: string): Promise<BigNumber> => {
-	console.log('fetching tokenId');
 	const contract = await listingContract(address);
-	console.log('fetchTokenId contract initialized');
 	const tokenId = await contract.tokenId();
-	console.log('tokenId is: ', tokenId.toString());
 	return tokenId;
 };
 
 export const fetchListed = async (address: string): Promise<boolean> => {
-	console.log('fetching listed');
 	const contract = await listingContract(address);
-	console.log('fetchListed contract initialized');
 	const listed = await contract.listed();
-	console.log('listed is: ', listed);
 	return listed;
 };
 
--- src/routes/listing/[address]/+page.ts
+++ src/routes/listing/[address]/+page.ts
@@ -9,35 +9,12 @@ export const load = async ({ params }: LoadEvent) => {
 		throw new Error('No ID found');
 	}
 
-	console.log('listing address: ', address);
-
 	const [price, seller, tokenId, listed] = await Promise.all([
 		fetchPrice(address),
 		fetchSeller(address),
 		fetchTokenId(address),
 		fetchListed(address)
 	]);
-
-	console.log('listing contract info successfully fetched');
-	console.log('fetching midi info');
-
-	// const price = await fetchPrice(address);
-	// console.log('price is: ', price);
-	// const seller = await fetchSeller(address);
-	// console.log('seller is: ', seller);
-	// const tokenId = await fetchTokenId(address);
-	// console.log('tokenId is: ', tokenId);
-	// const listed = await fetchListed(address);
-	// console.log('listed is: ', listed);
-
-	// const availableAmount = await fetchBalanceOf(address, tokenId.toNumber());
-	// console.log('availableAmount is: ', availableAmount.toString());
-
-	// const totalAmount = await fetchTotalReceived(address, tokenId.toNumber());
-	// console.log('totalAmount is: ', totalAmount.toString());
-
-	// const ipfsMetadata = await get(tokenId.toString());
-	// console.log('ipfsMetadata is: ', ipfsMetadata);
 
 	const [availableAmount, totalAmount, ipfsMetadata] = await Promise.all([
 		fetchBalanceOf(address, tokenId.toNumber()),
